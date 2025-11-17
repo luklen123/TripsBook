e43d2274cf1a0a04d79392a3caaa21c189dba3bf
liwiadzz <liwiaa7@gmail.com>
2025-11-17 16:32:59 +0100

    Create Trip.swift

---
+++ Trip.swift
@@ -0,0 +1,22 @@
+import Foundation
+import SwiftUI
+
+struct Trip: Identifiable, Codable {
+    let id = UUID()
+    
+    var country: String           // 
+    var cities: [String]          // czy jakos weryfikujemy
+    var startDate: Date
+    var endDate: Date
+    
+    var status: TripStatus        
+    var notes: String             
+    var photos: [String]          // to trzeba na macu bedzie
+}
+
+enum TripStatus: String, Codable {
+    case planned = "W planie"
+    case ongoing = "W trakcie"
+    case completed = "Zakończona"
+}
+
